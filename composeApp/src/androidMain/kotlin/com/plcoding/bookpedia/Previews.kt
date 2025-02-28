f84fb62af7677528dc12b25891d6816de508e2f6
Babatunde Owoleke <tundeowoleke1@gmail.com>
2025-02-28 17:26:45 +0100

    feat: Implement book list screen

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -5,6 +5,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.book_list.BookListState
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview
@@ -13,5 +16,29 @@ private fun BookSearchBarPreview() {
     MaterialTheme {
 
         BookSearchBar("Kotlin", {}, {}, Modifier.fillMaxWidth())
+    }
+}
+val books = (1..10).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Babatunde Owoleke"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = "2025",
+        averageRating = 4.6754,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 4
+
+
+    )
+}
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(state = BookListState(searchResults = books)) {
+
     }
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -5,9 +5,27 @@ import com.plcoding.bookpedia.core.presentation.UiText
 
 data class BookListState(
     val searchQuery: String = "Kotlin",
-    val searchResults: List<Book> = emptyList(),
+    val searchResults: List<Book> = books,
     val favoriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null,
 )
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Babatunde Owoleke"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = "2025",
+        averageRating = 4.6754,
+        ratingCount = 5,
+        numPages = 100,
+        numEditions = 4
+
+
+    )
+}
